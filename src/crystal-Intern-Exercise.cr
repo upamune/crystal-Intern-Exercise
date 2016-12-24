5026317188fe577d07c26607c57e0248683c1ab1
upamune <jajkeqos@gmail.com>
2016-12-24 23:08:42 +0900

    init commit

---
+++ src/crystal-Intern-Exercise.cr
@@ -0,0 +1,5 @@
+require "./crystal-Intern-Exercise/*"
+
+module Crystal::Intern::Exercise
+  # TODO Put your code here
+end
---
+++ src/crystal-Intern-Exercise/version.cr
@@ -0,0 +1,3 @@
+module Crystal::Intern::Exercise
+  VERSION = "0.1.0"
+end
