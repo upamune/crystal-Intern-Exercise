1532133fed81bb1728258b34a7aa2c375039ffea
upamune <jajkeqos@gmail.com>
2016-12-26 17:38:07 +0900

    add splited req

--- src/crystal-Intern-Exercise/log.cr
+++ src/crystal-Intern-Exercise/log.cr
@@ -1,19 +1,21 @@
   class Log
+  @splited_req : Array(String)
   getter :host, :user, :epoch, :req, :status, :size, :referer
 
   def initialize(@host : String, @user : (String | Nil), @epoch : Int32, @req : String, @status : Int32, @size : Int32, @referer : (String | Nil))
+    @splited_req = @req.split(" ")
   end
 
   def method : String
-    @req.split(" ")[0]
+    @splited_req[0]
   end
 
   def path : String
-    @req.split(" ")[1]
+    @splited_req[1]
   end
 
   def protocol : String
-    @req.split(" ")[2]
+    @splited_req[2]
   end
 
   def time : String
